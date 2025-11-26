28741857c9444069f17a4c1c28d90796eae2ef2d
Byoungcheon <lbcsultan@gmail.com>
2025-11-26 12:47:42 +0900

    05. list products

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,15 @@
+import ProductList from '@/components/ui/shared/product/product-list'
+import sampleData from '@/lib/sample-data'
+import { Product } from '@/types'
 export default function Home() {
-  return <div>Latest Products</div>
+  const validatedProducts: Product[] = sampleData.products.map((product) => ({
+    ...product,
+    isFeatured: product.isFeatured ?? false,
+  }))
+  return (
+    <div className="space-y-8">
+      <h2 className="h2-bold">Latest Products</h2>
+      <ProductList data={validatedProducts} />
+    </div>
+  )
 }
